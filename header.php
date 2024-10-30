fb42b81525a216aca67ef297389f770bb39801a7
TGr8Prtndr <154078660+TGr8Prtndr@users.noreply.github.com>
2024-10-30 18:34:35 +0100

    Add files via upload
    Header et footer (provisoire ?) du site web.

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $titre; ?></title>
+  <link rel="stylesheet" href="navigation_menu.css">
+  <link rel="stylesheet" href="layout.css">
+  <link rel="stylesheet" href="index.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- Bibliothèque Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Delius&family=Moon+Dance&display=swap" rel="stylesheet">
+
+</head>
+
+<!-- Ajout d'un scrollspy pour la navigation sur la page -->
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50">
